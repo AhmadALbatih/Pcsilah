e335512967e7fe6f62a30a436d12662ba535a3ff
saleeem123 <98378349+saleeem123@users.noreply.github.com>
2022-01-27 12:46:25 +0300

    add

---
+++ app/src/main/java/com/example/pcsilah/demo_gpu.java
@@ -0,0 +1,14 @@
+package com.example.pcsilah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class demo_gpu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_gpu);
+    }
+}
---
+++ app/src/main/java/com/example/pcsilah/demo_ram.java
@@ -0,0 +1,14 @@
+package com.example.pcsilah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class demo_ram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_ram);
+    }
+}

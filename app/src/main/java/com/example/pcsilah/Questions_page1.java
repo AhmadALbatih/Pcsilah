1e0f56aaa255d55917eb95d716bb6d6d6941f3e6
ahmad <ahmadalbatih@gmail.com>
2022-02-09 08:29:48 +0300

    Questions

---
+++ app/src/main/java/com/example/pcsilah/Questions_page1.java
@@ -0,0 +1,14 @@
+package com.example.pcsilah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Questions_page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_page1);
+    }
+}

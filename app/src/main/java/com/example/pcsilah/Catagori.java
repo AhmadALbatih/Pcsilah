d47247bf5fc727c4524fc9598f81b53c68760eec
ahmad <ahmadalbatih@gmail.com>
2022-02-15 08:42:27 +0300

    d

--- app/src/main/java/com/example/pcsilah/Catagori.java
+++ app/src/main/java/com/example/pcsilah/Catagori.java
@@ -1,7 +1,15 @@
 package com.example.pcsilah;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.ImageView;
 import android.os.Bundle;
 
 public class Catagori extends AppCompatActivity {

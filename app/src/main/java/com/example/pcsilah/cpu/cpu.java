738e229f0ce7516124c2431de7a29872beda1bad
troll675 <Anq100753@hotmail.com>
2022-02-22 16:13:06 +0300

    add cpu
    d

--- app/src/main/java/com/example/pcsilah/cpu/cpu.java
+++ app/src/main/java/com/example/pcsilah/cpu/cpu.java
@@ -20,10 +20,19 @@ public class cpu extends AppCompatActivity {
 		setContentView(R.layout.cpu);
 		cpuList = new ArrayList<>();
 		cpuList.add(new cpuclass("Intel", "i3-8100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
-		cpuList.add(new cpuclass("Intel", "i3-8100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
-		cpuList.add(new cpuclass("Intel", "i3-8100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
-		cpuList.add(new cpuclass("Intel", "i3-8100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
-		cpuList.add(new cpuclass("Intel", "i3-8100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i5-8500","6 cores,","3.00 GHz","9 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i7-8700","6 cores,","3.20 GHz","12MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i3-9100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i5-9500","6 cores,","3.00 GHz","9 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i7-9700","8 cores,","3.00 GHz","12MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i3-10100","4 cores,","3.60 GHz","6 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i5-10500","6 cores,","3.10 GHz","12 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Intel", "i7-10700","8 cores,","2.90 GHz","16 MB Intel® Smart Cache","Intel® UHD Graphics 630","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Ryzen", "7 1700","8 cores,","3.00 GHz ","16 MB L3 Cache","No Integrated Graphics","65 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Ryzen", "9 5950X","16 Cores,","3.4 GHz","64 MB L3","No Integrated Graphics","105 W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Ryzen", "7 5700G","8-Core,","3.8GHz","16MB L3","Radeon™ Graphics","45-65W",R.drawable.cpu));
+		cpuList.add(new cpuclass("Ryzen", "5 5600G","6-Core,","3.90GHz","16MB L3","Radeon™ Graphics","65W",R.drawable.cpu));
+
 		cpuAdapter = new CpuAdapter(this, 0, cpuList);
 		ListView listView = (ListView) findViewById(R.id.names_list_view);
 		listView.setAdapter(cpuAdapter);

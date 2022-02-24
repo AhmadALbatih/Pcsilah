7b8a85e2fae82432057798c92eaa7ae558d302e2
ahmad <ahmadalbatih@gmail.com>
2022-02-24 12:58:47 +0300

    d

--- app/src/main/java/com/example/pcsilah/ram/ram.java
+++ app/src/main/java/com/example/pcsilah/ram/ram.java
@@ -20,7 +20,7 @@ public class ram extends AppCompatActivity {
 		setContentView(R.layout.ram);
 		ramList = new ArrayList<>();
 		ramList.add(new ramclass("Patriot Memory", "4400 MHz","(2X8) 16 GB","DDR4","1.45 Volts", R.drawable.cpu));
-		ramList.add(new ramclass("Corsair", "3600 MHz","16 GB","DDR4","1.35 Volts", R.drawable.cpu));
+		ramList.add(new ramclass("Corsair", "3600 MHz","16 GB","DDR4","1.35 Volts", R.drawable.corsair));
 		ramList.add(new ramclass("Crucial", "2666 MHz","64 GB","DDR4","1.2 Volts", R.drawable.cpu));
 		ramList.add(new ramclass("TEAMGROUP", "3600 MHz","16 GB","DDR4","1.35 Volts", R.drawable.cpu));
 		ramList.add(new ramclass("TUF Gaming Alliance (TEAMGROUP)", "3000 MHz","16 GB","DDR4","1.35 Volts", R.drawable.cpu));
